14e60e11f228f278b027aa1649e06721af1c6e83
sushantm1 <sushantm1100202@gmail.com>
2025-09-01 14:21:38 +0530

    new prgms

---
+++ BillGeneratore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BillGeneratore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Invoice
+    {
+        string customerName;
+        int totalItems;
+        int totalAmount;
+        int gst;
+        int finalAmount;
+
+    }
+    class customer
+    {
+        string name;
+        int id;
+        //string address;
+        //int 
+    }
+    class 
+}
