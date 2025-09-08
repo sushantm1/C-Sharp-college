71358f8569f24725d2b98af58942f3b8ad35be3f
sushantm1 <sushantm1100202@gmail.com>
2025-09-08 22:42:32 +0530

    new assign ment

---
+++ Assign-4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account= new BankAccount();
+            account.Deposit(1000);
+            Console.ReadLine();
+
+        }
+
+    }
+    class BankAccount
+    {
+        int balance;
+        
+        public void Deposit(int amount) {
+            this.balance += amount;
+            Console.WriteLine("this amount has been deposited : "+ amount);
+            Console.WriteLine("so the current balance in your account is : " + balance);
+        }
+
+    }
+}
