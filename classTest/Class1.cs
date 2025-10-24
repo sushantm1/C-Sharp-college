ba8a445c729c035358e6372c9ce0ea2364a02ea0
sushantm1 <sushantm1100202@gmail.com>
2025-10-24 19:40:11 +0530

    lab assisgements

---
+++ classTest/Class1.cs
@@ -0,0 +1,45 @@
+﻿////using System;
+////using System.Collections.Generic;
+////using System.Linq;
+////using System.Text;
+////using System.Threading;
+////using System.Threading.Tasks;
+
+////namespace classTest
+////{
+////    internal class Class1
+////    {
+////    }
+////}
+
+
+////ḍmsa
+
+//using System;
+//using System.Threading;
+
+//class Program
+//{
+//    static void PrintNumbers()
+//    {
+//        for (int i = 1; i <= 5; i++)
+//        {
+//            Console.WriteLine("Worker Thread: " + i);
+//            Thread.Sleep(0);
+//        }
+//    }
+
+//    static void Main()
+//    {
+//        Thread t1 = new Thread(PrintNumbers);
+//        t1.Priority = ThreadPriority.AboveNormal;
+//        t1.Start();
+
+//        for (int i = 1; i <= 5; i++)
+//        {
+//            Console.WriteLine("Main Thread: " + i);
+//            Thread.CurrentThread.Priority = ThreadPriority.BelowNormal;
+//            Thread.Sleep(0);
+//        }
+//    }
+//}
---
+++ classTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading;
+
+class Program
+{
+    static void PrintNumbers()
+    {
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Worker Thread: " + i);
+            Thread.Sleep(0);
+        }
+    }
+
+    static void printAlphabates()
+    {
+        for (char c = 'A'; c <= 'Z'; c++)
+        {
+            Console.WriteLine("Alphabet thread : " + c);
+            Thread.Sleep(0);
+        }
+    }
+    static void Main()
+    {
+        Thread t1 = new Thread(PrintNumbers);
+        t1.Priority = ThreadPriority.AboveNormal;
+        t1.Start();
+
+        Thread t2 = new Thread(printAlphabates);
+        t2.Priority = ThreadPriority.AboveNormal;
+        t2.Start();
+
+        for (int i = 1; i <= 5; i++)
+        {
+            Console.WriteLine("Main Thread: " + i);
+            Thread.CurrentThread.Priority = ThreadPriority.BelowNormal;
+            Thread.Sleep(0);
+        }
+    }
+}
