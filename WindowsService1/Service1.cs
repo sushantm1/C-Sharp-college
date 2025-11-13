334498a0037466019fa5df3312846279f93650b7
sushantm1 <sushantm1100202@gmail.com>
2025-11-13 13:14:27 +0530

    windows service

---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            File.AppendAllText(@"C:MCAWindowService.txt", "Service created by on :");
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
